747631a572c2a9a3a9c838ce611c581231f11e6f
Moaz Gabat <moaz.galbat@gmail.com>
2024-06-02 14:58:24 +0300

    added helmet for security

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,15 +3,18 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Config } from './config/config.interface';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
   );
+  //empty of development
   app.enableCors();
   const configService = app.get(ConfigService<Config>);
   const appConfig = configService.get('app');
